1782dec49b3d4cd3a87006d3370fbca48e04d5e1
Cristina Pérez <cristinaperezhuerta@gmail>
2025-07-01 23:35:51 +0200

    implement page not found

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -37,87 +37,21 @@ server.listen(port, () => {
 });
 
 server.get("/api/projects", async (req, res) => {
-  // 1. Conectarnos a la base de datos
-
-  const conn = await getConnection();
-
-  // 2. Lanzamos un SELECT y recuperamos los datos como JSON
-
-  const [results] = await conn.query(
-    `SELECT *
-      FROM proyectos p
-      JOIN authors a ON (p.id = a.fk_projects);`
-  );
-
-  // 3. Cerramos la conexión
-
-  await conn.end();
-
-  // 4. Devolvemos en la respuesta (res) el JSON
-
-  res.json(results);
-
-  /*  res.sendFile(path.join(__dirname, "../FRONTEND/src/data/projects.json")); */
-  /*
-  const results = [
-    {
-      name: "Proyecto 1",
-      slogan: "Slogan",
-      desc: "Bienvenidos a mi proyecto molón Cande",
-      technologies: "Blender, GameMarker, Unity3D",
-      demo: "https://github.com/Nelicah",
-      repo: "",
-      autor: "Cande",
-      job: "Diseñadora de videojuegos molones",
-      photo: "https://i.blogs.es/7f0b02/lol/840_560.jpg",
-      image: "https://avatars.githubusercontent.com/u/199109765?v=4",
-    },
-
-    {
-      name: "Proyecto 2",
-      slogan: "Slogan",
-      desc: "Bienvenidos a mi proyecto molón Jessica",
-      technologies: "React, JavaScript, HTML",
-      demo: "https://github.com/JessicaVR86",
-      repo: "",
-      autor: "Jessica",
-      job: "Diseñadora web Frontend",
-      photo:
-        "https://www.enfoquegaussiano.com/wp-content/uploads/mockup-web-4.jpg",
-      image: "https://avatars.githubusercontent.com/u/204056900?v=4 ",
-    },
-
-    {
-      name: "Proyecto 3",
-      slogan: "Slogan",
-      desc: "Bienvenidos a mi proyecto molón Raquel",
-      technologies: "CSS, Sass, Figma",
-      demo: "https://github.com/RaquelPZ",
-      repo: "",
-      autor: "Rachel",
-      job: "Diseñadora UX/UI",
-      photo:
-        "https://img.freepik.com/vector-premium/banner-conceptos-palabra-diseno-ux-interfaz-usuario-integracion-productos-infografia-iconos-lineales-sobre-fondo-verde-tipografia-creativa-aislada-ilustracion-color-contorno-vectorial-texto_106317-5864.jpg",
-      image: "https://avatars.githubusercontent.com/u/206062240?v=4",
-    },
-
-    {
-      name: "Proyecto 4",
-      slogan: "Slogan",
-      desc: "Bienvenidos a mi proyecto molón Chrissy",
-      technologies: "React, Node.js, MySQL Workbench",
-      demo: "https://github.com/Chrissy-92",
-      repo: "",
-      autor: "Chrissy P.",
-      job: "Diseñadora web Fullstack",
-      photo:
-        "https://www.ldhmsoftware.com/wp-content/uploads/2023/11/0_cl7fc6pt1MHjIF4K-1024x622.png",
-      image: "https://avatars.githubusercontent.com/u/185409586?v=4",
-    },
-  ];
-
-  res.json(results);
-  */
+  try {
+    // 1. Conectarnos a la base de datos
+    const conn = await getConnection();
+    // 2. Lanzamos un SELECT y recuperamos los datos como JSON
+    const [results] = await conn.query(
+      `SELECT * FROM proyectos p JOIN authors a ON (p.id = a.fk_projects);`
+    );
+    // 3. Cerramos la conexión
+    await conn.end();
+    // 4. Devolvemos en la respuesta (res) el JSON
+    res.json(results);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Error al obtener proyectos" });
+  }
 });
 
 server.post("/api/projects", async (req, res) => {
@@ -210,6 +144,8 @@ server.post("/api/projects", async (req, res) => {
   console.log(cardURL);
 });
 
+// SERVIDOR DE PLANTILLAS
+
 server.get("/api/projects/:id", async (req, res) => {
   // 1. Conectarnos a la base de datos
 
@@ -250,10 +186,16 @@ server.get("/projects/:id", async (req, res) => {
     `SELECT * FROM proyectos p JOIN authors a ON (p.id = a.fk_projects) WHERE p.id = ?;`,
     [req.params.id]
   );
+  console.log(results);
 
-  await conn.end();
+  if (results.length === 0) {
+    res.render("not_found");
+    return;
+  }
 
   const projectData = results[0];
+
+  await conn.end();
 
   res.render("detail", projectData);
 });
